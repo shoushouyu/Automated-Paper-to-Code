8733345ddbcac32fb6f4261968b8a800f766078f
shoushouyu <85824696+shoushouyu@users.noreply.github.com>
2025-04-27 22:14:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,12 @@ python3 content_process/picture.py && \
 python3 content_process/md_process.py && \        
 python3  design/cot_eng.py &&\
 python3 design/class_design.py &&\
+python3 code_gen/merge.py &&\
+python3 code_gen/last_modified.py &&\
+python3 code_gen/code_generate.py &&\
+python3 code_gen/code_fix.py &&\
+python3 code_gen/alignment.py &&\
+python3 code_gen/finetune.py
 
 
 echo "All scripts executed successfully."
