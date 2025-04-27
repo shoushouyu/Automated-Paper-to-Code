c07aa12ba623529ba1a1675f72d612f797b55521
shoushouyu <85824696+shoushouyu@users.noreply.github.com>
2025-04-27 22:11:14 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ set -e
 
 python3 content_process/picture.py && \
 python3 content_process/md_process.py && \        
-python3  
+python3  design/cot_eng.py &&\
+python3 design/class_design.py &&\
 
 
 echo "All scripts executed successfully."
